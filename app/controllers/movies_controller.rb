992dea352fb3f69f9989fcb552ebb2d5d48ea06f
shinpotato <potatoandtomato2@gmail.com>
2020-02-08 07:21:34 +0900

    add_login_func_to_navbar_and_improve_the_layout

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,16 @@ class MoviesController < ApplicationController
 
   #Railsの動画教材ページ（＝トップページ）へ
   def index
+
+    if user_signed_in?
+
+    else
+
+      redirect_to(new_user_session_path)
+
+    end
   end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :users
   ActiveAdmin.routes(self)
   root to: 'movies#index'
+
+
 end
