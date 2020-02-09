f3e8854cd87b69e8aa0106cc6064f4fb0034c92b
shinpotato <potatoandtomato2@gmail.com>
2020-02-09 12:25:52 +0900

    SmallFix:use_aunthenticate_user_and_add_partial_template

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,7 @@
 class MoviesController < ApplicationController
-
+  before_action :authenticate_user!
   #Railsの動画教材ページ（＝トップページ）へ
   def index
-
-    if user_signed_in?
-
-    else
-
-      redirect_to(new_user_session_path)
-
-    end
   end
 
 
